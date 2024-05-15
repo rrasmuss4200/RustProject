10e391f4b738d063166e721a405febdff60c8d86
rrasmuss07 <rrasmuss@ualberta.ca>
2024-05-15 15:48:19 -0600

    Singe Thread Working :)

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -18,24 +18,15 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     // manages calls to std::io::Read
     let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // collects the lines that the request from the browser generates and puts them into a vector
-    let http_request: Vec<_> = buf_reader
-        // returns an iterator that splits each time it reads a new line character
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    // an HTTP successful request response. Communicates HTTP version 1.1,
-    // a statues code of 200 (success), an OK reason phrase, no headers and no body
-    let status_line = "HTTP/1.1 200 OK";
-
-    // uses filesystem library to read contents of file
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
     stream.write_all(response.as_bytes()).unwrap();
-}
+        }
