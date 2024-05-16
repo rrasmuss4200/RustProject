cfa34dcea8b45ab77827f49d054eec34239eaa40
rrasmuss07 <rrasmuss@ualberta.ca>
2024-05-16 14:14:52 -0600

    Compiles using Arc and Mutex to share messages across channels

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,27 +1,38 @@
-use std::thread::{self, JoinHandle};
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
+// holds the threads awaiting to execute code
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
 
 impl ThreadPool {
     /// Create a new Thread Pool
-    /// The size if the number of threads in the pool
+    /// The size if the number of threads in the pool.
+    /// This is also the number of Workers
     ///
     /// #Panics
     /// 'new' will panic if size is 0 or negative
-    pub fn new(size: usize) -> ThreadPool{
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
 
-        for _ in 0..size {
+        let receiver = Arc::new(Mutex::new(receiver));
 
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool {threads}
+        ThreadPool { workers, sender }
     }
-
+    // will send a job from the ThreadPool to the Worker instances, which will send the job to its thread
     pub fn execute<F>(&self, f: F)
     where
     // this is how you take in a closure as a parameter
@@ -29,4 +40,27 @@ impl ThreadPool {
         {
 
         }
+}
+
+// item that is sent down the chnnel
+struct Job;
+
+// picks up code that is to be run and runs it in the Workers thread
+// this allows the threads in ThreadPool to WAIT for code that will be sent later
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // this is the thread that 'holds' the code for the pool
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker {
+            id,
+            thread
+        }
+    }
 }
