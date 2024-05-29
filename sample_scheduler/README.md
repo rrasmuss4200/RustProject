0e9d5273f0e71dc2b2714e9c6fa7b949306b75cc
rrasmuss07 <rrasmuss@ualberta.ca>
2024-05-29 16:12:13 -0600

    Allows human-readable timestamp to be compared with Unix Epoch in order to schedule execution.

--- sample_scheduler/README.md
+++ sample_scheduler/README.md
@@ -1,2 +1,5 @@
 # Sample Scheduelr
 This is the start of a scheduler that I am building to handle a command, assign it a time then execute it at that time. The command is a tuple of (time,command).
+***IMPORTANT:*** The scheduler expects time arguments in UTC to compare the current time with the time of the command.
+For now, the command is the first command line argument and the time of execution is the second.
+**TIME OF EXECUTION** is expected in this format: *YYYY-MM-DD HH:MM:SS*
--- sample_scheduler/src/main.rs
+++ sample_scheduler/src/main.rs
@@ -1,11 +1,12 @@
-use std::env;
+use std::{env, time::{SystemTime, Duration}};
 use log::{error, info, debug};
 use log4rs;
+use std::time;
+use chrono::{NaiveDateTime, TimeZone, Utc};
 
 // enum of different states of a process
 pub enum MessageState {
     New,
-    Ready,
     Suspended,
     Waiting,
     Running,
@@ -14,36 +15,52 @@ pub enum MessageState {
 
 
 pub struct Message {
+    time: Result<u64, String>,
     state: MessageState,
     id: u32,
-    len: u8,
-    dest: u8,
-    opcode: u8,
-    data: u32,
+    command: String,
 }
 
 fn handle_state(msg: Message) {
     match msg.state {
         MessageState::New => {
-            info!("New task received. #{}", msg.id);
-        }
-        MessageState::Ready => {
-            info!("Task #{} ready to execute.", msg.id);
+            info!("New task received at {:?}. #{}",msg.time, msg.id);
         }
         MessageState::Running => {
             info!("Task #{} is running.", msg.id);
+            // Send to CmdDispatcher
         }
         MessageState::Done => {
             info!("Task #{} is done running.", msg.id)
+            // Acknowledgement from CmdDispatcher
         }
         MessageState::Waiting => {
             info!("Task #{} is waiting to run.", msg.id);
+            // Put into CmdDispatcher buffer
         }
         MessageState::Suspended => {
             info!("Task #{} is suspended.", msg.id);
+            // Put into non-volatile memory
         }
     }
 }
+
+fn timestamp_to_epoch(timestamp: String) -> Result<u64, String> {
+    // RETURNS VALUE IN MILLISECONDS
+    // Parse the input timestamp using NaiveDateTime from the chrono crate
+    match NaiveDateTime::parse_from_str(&timestamp, "%Y-%m-%d %H:%M:%S") {
+        Ok(naive_date_time) => {
+            // Convert the NaiveDateTime to a DateTime<Utc> object
+            let date_time_utc = Utc.from_utc_datetime(&naive_date_time);
+            // Get the epoch timestamp in milliseconds
+            let epoch_millis = date_time_utc.timestamp_millis();
+            // Convert the epoch timestamp to u64
+            Ok(epoch_millis as u64)
+        }
+        Err(e) => Err(format!("Failed to parse timestamp: {}", e)),
+    }
+}
+
 fn main() {
     debug!("Initializing Logger.");
     if let Err(e) = log4rs::init_file("log4rs.toml", Default::default()) {
@@ -53,31 +70,46 @@ fn main() {
     }
 
     let args: Vec<String> = env::args().collect();
+    let command_arg: String = args[1].parse::<String>().unwrap();
+    // YYYY-MM-DD HH:MM:SS
+    let human_date: String = args[2].parse::<String>().unwrap();
 
-    // Template for time:
-    // YYYY-MM-DDTHH:MM:SS
-    // the T is part of the message. You put it in
-    // let time_arg: String = args[1].parse::<String>().unwrap();
+    // Convert input human-readalbe time to epoch time to compare times when program is run
+    let command_time: Result<u64, String> = timestamp_to_epoch(human_date);
 
-    // let command_arg: String = args[2].parse::<String>().unwrap();
+    // while true to keep current time updating
 
-    // let input_tuple: (String, String) = (time_arg,command_arg);
-
-    // let (time,command) = input_tuple;
-    // println!("Time: {}, Command: {}", time, command);
-
-    let msg = Message {
-        state: MessageState::New,
-        id: 0,
-        len: 35,
-        dest: 1,
-        opcode: 4,
-        data: 32,
+    let curr_time: Result<Duration, time::SystemTimeError> = time::SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
+    let curr_time_millis: u64 = match curr_time {
+        Ok(duration) => duration.as_millis() as u64,
+        Err(e) => {eprint!("Error {:?}", e);
+    return;}
     };
 
-    handle_state(msg);
+    let now: Result<u64, String> = command_time.clone();
 
+    let input_tuple: (Result<u64, String>, String) = (now.clone(),command_arg.clone());
 
-    // create queue of incoming tasks. Assign priority values
+    println!("Command Time: {:?} ms, Command: {}\nUnix Epoch is {:?} ms", input_tuple.0.unwrap(), input_tuple.1, curr_time_millis);
+
+    let mut msg = Message {
+        time: command_time.clone(),
+        state: MessageState::New,
+        id: 0,
+        command: command_arg,
+    };
+
+    if msg.time >= Ok(curr_time_millis) {
+        // Send to CmdDispathcer
+        msg.state = MessageState::Running;
+        handle_state(msg);
+        println!("Sent to CmdDispatcher");
+    } else {
+        error!("Command is of type 'now'. Should have been dispatched.")
+    }
+
+    // TODO: create queue of incoming tasks. Assign priority values and sort based on time to be execcuted
+    // can create function for comparing UTC epoch values that returns true if ones before he other
+    // and an if statement uses this to determine whether the values will be swapped
 
 }
